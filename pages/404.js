f3fa0f4af5dccbc61a096f63863551e0166538ba
John Paulo Cruz <54509205+paupaugo@users.noreply.github.com>
2022-09-20 13:09:35 +0800

    fix 404

--- pages/404.js
+++ pages/404.js
@@ -7,36 +7,13 @@ import { useRouter } from 'next/router';
 
 
 const Custom404 = () => {
-	const router = useRouter();
+	const router = useRouter()
 
+  useEffect(() => {
+    router.replace("/")
+  })
 
-
-useEffect(() => {
- 
-	window.location.href("/");
- 
-},[]);
-    return (
-		<div className="error-area">
-			<div className="d-table">
-				<div className="d-table-cell">
-					<div className="container">
-						<div className="error-content">
-							<div className="notfound-404">
-								<h1>Oops!</h1>
-							</div>
-							<h3>404 - Page not found</h3>
-							<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-
-							<Link href="/">
-								<a className="btn btn-primary">Go to Homepage</a>
-							</Link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-    )
+  return null
 }
 
 export default Custom404
