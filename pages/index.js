9eb428384830e24c8de3b57a3ef4defc8c280afb
John Paulo Cruz <54509205+paupaugo@users.noreply.github.com>
2022-09-20 09:52:35 +0800

    fix index.js

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,12 @@ import Navbar from "@/components/_App/Navbar";
 import MainBanner from "@/components/ITStartup/MainBanner";
 import Features from "@/components/ITStartup/Features";
 import OurFeatures from "@/components/ITStartup/OurFeatures";
-// import ServicesArea from "@/components/ITStartup/ServicesArea";
 import Team from "@/components/Common/Team";
 import FunFactsArea from "@/components/Common/FunFactsArea";
-// import RecentWorks from "@/components/Common/RecentWorks";
-// import PricingStyleOne from "@/components/PricingPlans/PricingStyleOne";
-// import Feedback from "@/components/Common/Feedback";
-// import Partner from "@/components/Common/Partner";
-// import BlogPost from "@/components/Common/BlogPost";
+
 import Footer from "@/components/_App/Footer";
 import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
-import ComingSoon from ""
+
 
 const Index = () => {
     return (
